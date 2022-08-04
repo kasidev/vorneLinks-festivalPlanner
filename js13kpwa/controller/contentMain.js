37f6ecc246f5ff9463622c204b856586a07f6fc8
Patrick Kasiala <dev@kasiala.ch>
2022-08-04 13:09:30 +0200

    start with searchbar

--- js13kpwa/controller/contentMain.js
+++ js13kpwa/controller/contentMain.js
@@ -4,61 +4,63 @@ import {add2Favorites} from "./saveAct.js";
 import {removeFavorites} from "./deleteAct.js";
 
 export function contentMain(){
-    // Generating content based on the template
-    const actsSorted = acts.sort(function(a,b){return a.start-b.start})
-    const template = `<div class="act">
-        <div class="actName">
-            <h3>ACT_NAME</h3>
-        </div>
-        <article>
-            <div class="actData">
-                <ul>
-                <li>TYPE</li>
-                <li><span>FROM</span> - <span>TO</span> @ <span>WHERE</span></li>
-                </ul>
+    function renderMainPage(acts2Display) {
+        // Generating content based on the template
+        const actsSorted = acts2Display.sort(function(a,b){return a.start-b.start})
+        const template = `<div class="act">
+            <div class="actName">
+                <h3>ACT_NAME</h3>
             </div>
+            <article>
+                <div class="actData">
+                    <ul>
+                    <li>TYPE</li>
+                    <li><span>FROM</span> - <span>TO</span> @ <span>WHERE</span></li>
+                    </ul>
+                </div>
 
-            <div class="actButtons">
-                <button class="round-btn" id="info_ID" data-id="ID">info</button>
-                <button class="round-btn" id="save_ID" data-id="ID">save</button>
-                <button class="round-btn"><a href='MFW_LINK'>MFW</a></button>
-                <button class="round-btn" id="friends_ID" data-friendsCount=FRIENDS data-friendsList=FLIST>FRIENDS</button>
-            </div>
-        </article>
-    </div>`;
-    let content = '';
-    for (let i = 0; i < actsSorted.length; i++) {
-        if (actsSorted[i].start>=localStorage.startTime && actsSorted[i].start<=localStorage.endTime ||
-            actsSorted[i].name) {
-            let friendsCount=0
-            let friendsList = ""
-            for (const friend of JSON.parse(localStorage.friends)) {
-                //console.log("friend",friend)
-                for (const favorites of friend.favorites) {
-                    if (favorites == i) {
-                        friendsCount=friendsCount+1
-                        friendsList += (friend.friendName+", ")
-                        console.log("friendslist",friendsList)
-                    }                
+                <div class="actButtons">
+                    <button class="round-btn" id="info_ID" data-id="ID">info</button>
+                    <button class="round-btn" id="save_ID" data-id="ID">save</button>
+                    <button class="round-btn"><a href='MFW_LINK'>MFW</a></button>
+                    <button class="round-btn" id="friends_ID" data-friendsCount=FRIENDS data-friendsList=FLIST>FRIENDS</button>
+                </div>
+            </article>
+        </div>`;
+        let content = '';
+        for (let i = 0; i < actsSorted.length; i++) {
+            if (actsSorted[i].start>=localStorage.startTime && actsSorted[i].start<=localStorage.endTime ||
+                actsSorted[i].name) {
+                let friendsCount=0
+                let friendsList = ""
+                for (const friend of JSON.parse(localStorage.friends)) {
+                    //console.log("friend",friend)
+                    for (const favorites of friend.favorites) {
+                        if (favorites == i) {
+                            friendsCount=friendsCount+1
+                            friendsList += (friend.friendName+", ")
+                            console.log("friendslist",friendsList)
+                        }                
+                    }
                 }
+                let entry = template.replace(/POS/g, (i + 1))
+                    .replace(/ACT_NAME/g, actsSorted[i].name)
+                    .replace(/TYPE/g, actsSorted[i].style)
+                    .replace(/MFW_LINK/g, actsSorted[i].mfwLink)
+                    .replace(/FROM/g, moment.unix(actsSorted[i].start).format("HH:mm"))
+                    .replace(/TO/g, moment.unix(actsSorted[i].end).format("HH:mm"))
+                    .replace(/WHERE/g, stages[actsSorted[i].location-1].name)
+                    .replace(/ID/g, actsSorted[i].id)
+                    .replace(/FRIENDS/g, friendsCount)
+                    .replace(/FLIST/g, friendsList);
+                    
+                    
+                entry = entry.replace('<a href=\'http:///\'></a>', '-');
+                content += entry;
+                
             }
-            let entry = template.replace(/POS/g, (i + 1))
-                .replace(/ACT_NAME/g, actsSorted[i].name)
-                .replace(/TYPE/g, actsSorted[i].style)
-                .replace(/MFW_LINK/g, actsSorted[i].mfwLink)
-                .replace(/FROM/g, moment.unix(actsSorted[i].start).format("HH:mm"))
-                .replace(/TO/g, moment.unix(actsSorted[i].end).format("HH:mm"))
-                .replace(/WHERE/g, stages[actsSorted[i].location-1].name)
-                .replace(/ID/g, actsSorted[i].id)
-                .replace(/FRIENDS/g, friendsCount)
-                .replace(/FLIST/g, friendsList);
-                
-                
-            entry = entry.replace('<a href=\'http:///\'></a>', '-');
-            content += entry;
-            
+        
         }
-    
     }
     const mainPage = document.getElementById('content-main');
     mainPage.innerHTML=content;
@@ -94,8 +96,8 @@ export function contentMain(){
         //console.log(term)
         
         if(term.length<3) {
-            matches=[]
-            return
+            let matches2=[]
+            return matches2
         }
         let matches = acts.filter((act)=>{
             const regex = new RegExp(`${term}`,`gi`)
@@ -108,15 +110,20 @@ export function contentMain(){
         
         if(matches.length>0){
             for (const actFound of matches) {
-                console.log(actFound)
+                //console.log(actFound)
             }
             
     
         }
+        return matches
     
     }
-    search("Folk",acts.sort(function(a,b){return a.start-b.start}))
-
+    const serachBar= document.getElementById("search")
+    serachBar.addEventListener('keyup',()=>{
+        const res= search(serachBar.value,acts.sort(function(a,b){return a.start-b.start}))
+        console.log(res)
+    })
+    
 
 
 }
