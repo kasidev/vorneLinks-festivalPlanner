f69578f0f815dda0b910e5d98a869622a2bba338
Patrick Kasiala <dev@kasiala.ch>
2022-08-05 16:39:54 +0200

    sw update after  font update

--- js13kpwa/sw.js
+++ js13kpwa/sw.js
@@ -12,7 +12,7 @@ self.importScripts('helper/moment.js');*/
 console.log("service worker test")
 
 // Files to cache
-const cacheName = 'vorneLinksApp_20220804_1916';
+const cacheName = 'vorneLinksApp_20220805_1639';
 const appShellFiles = [
   '/index.html',
   '/favorites.html',
