17a8661173fa7423767a42077802e4edbd626625
Patrick Kasiala <dev@kasiala.ch>
2023-07-23 23:16:47 +0200

    new stages order

--- js13kpwa/data/stages.js
+++ js13kpwa/data/stages.js
@@ -1,63 +1,63 @@
 var stages = [
     {
         name:   "Steibi",
-        id:     1,
-        lat:    47.49828,
-        lon:    8.72801,
-
-    },
-    {
-        name:   "Chileplatz",
-        id:     2,
-        lat:    47.49828,
-        lon:    8.72801,
-
-    },
-    {
-        name:   "Roulotte",
-        id:     3,
-        lat:    47.49828,
-        lon:    8.72801,
-
-    },
-    {
-        name:   "Startrampe",
-        id:     4,
-        lat:    47.49828,
-        lon:    8.72801,
-
-    },
-    {
-        name:   "Stadtchile",
-        id:     5,
-        lat:    47.49828,
-        lon:    8.72801,
-
-    },
-    {
-        name:   "Gwerbmuseum",
         id:     6,
         lat:    47.49828,
         lon:    8.72801,
 
     },
     {
-        name:   "Stadtbibliothek",
-        id:     7,
+        name:   "Chileplatz",
+        id:     4,
         lat:    47.49828,
         lon:    8.72801,
 
     },
     {
-        name:   "Albani",
+        name:   "Roulotte",
+        id:     2,
+        lat:    47.49828,
+        lon:    8.72801,
+
+    },
+    {
+        name:   "Startrampe",
+        id:     3,
+        lat:    47.49828,
+        lon:    8.72801,
+
+    },
+    {
+        name:   "Stadtchile",
         id:     8,
         lat:    47.49828,
         lon:    8.72801,
 
     },
     {
-        name:   "Krafti",
+        name:   "Gwerbmuseum",
+        id:     7,
+        lat:    47.49828,
+        lon:    8.72801,
+
+    },
+    {
+        name:   "Stadtbibliothek",
         id:     9,
+        lat:    47.49828,
+        lon:    8.72801,
+
+    },
+    {
+        name:   "Albani",
+        id:     5,
+        lat:    47.49828,
+        lon:    8.72801,
+
+    },
+    {
+        name:   "Krafti",
+        id:     1,
         lat:    47.49828,
         lon:    8.72801,
 
